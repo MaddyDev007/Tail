2fda6df94824ac2a089fd7212e70062a880d24b3
MaddyDev007 <ffvip6804@gmail.com>
2024-12-29 21:30:02 +0530

    feat: added dark mode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundColor : {
+        'dark': '#282828',
+      },
     },
   },
   plugins: [],
